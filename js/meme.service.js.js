922684a26e0ff70356c670c7ce0fc9a623e8e3c0
Yifaht Meir <yifahtmyers@gmail.com>
2024-02-28 14:09:45 +0200

    Add global vars event listeners and getpose funcs

--- js/meme.service.js.js
+++ js/meme.service.js.js
@@ -1,8 +1,8 @@
 'use strict'
 
-let gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] }]
+let gImgs = [{ id: 1, url: 'img/one size/1.jpg', keywords: ['funny', 'cat'] }]
 
-let gMeme = {
+let gMemes = [{
     selectedImgId: 5,
     selectedLineIdx: 0,
     lines: [
@@ -12,6 +12,71 @@ let gMeme = {
             color: 'red'
         }
     ]
-}
+}]
 
 let gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
+
+let gPen = { pos: null, isDown: false }
+let gLine = []
+const TOUCH_EVENTS = ['touchstart', 'touchmove', 'touchend']
+
+function getMeme() {
+    return gMemes[0]
+}
+
+function getImg() {
+    return gImgs[0]
+}
+
+function getPenPos() {
+    return gPen
+}
+
+function getEvPos(ev) {
+    let pos = { x: ev.offsetX, y: ev.offsetY }
+    if (TOUCH_EVENTS.includes(ev.type)) {
+        ev.preventDefault()
+        ev = ev.changedTouches[0]
+        pos = {
+            x: ev.pageX - ev.target.offsetLeft - ev.target.clientLeft,
+            y: ev.pageY - ev.target.offsetTop - ev.target.clientTop
+        }
+    }
+    return pos
+}
+
+function addListeners() {
+    addMouseListeners()
+    addTouchListeners()
+}
+
+function addMouseListeners() {
+    gElCanvas.addEventListener('mousedown', handleStart)
+    gElCanvas.addEventListener('mousemove', handleMove)
+    gElCanvas.addEventListener('mouseup', handleEnd)
+}
+
+function addTouchListeners() {
+    gElCanvas.addEventListener('touchstart', handleStart)
+    gElCanvas.addEventListener('touchmove', handleMove)
+    gElCanvas.addEventListener('touchend', handleEnd)
+}
+
+function handleStart(ev) {
+    gPen.pos = getEvPos(ev)
+    gPen.isDown = true
+    gLine.push(gPen.pos)
+    gCtx.beginPath()
+    gCtx.moveTo(gPen.pos.x, gPen.pos.y)
+}
+
+function handleMove(ev) {
+    if (!gPen.isDown) return
+    gPen.pos = getEvPos(ev)
+}
+
+function handleEnd(ev) {
+    gPen.pos = getEvPos(ev)
+    gPen.isDown = false
+    gCtx.closePath()
+}
