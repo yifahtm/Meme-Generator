5fb784b03af11aa750a91954a3475e7e0e725c20
Yifaht Meir <yifahtmyers@gmail.com>
2024-02-28 14:11:10 +0200

    Add init&render funcs and render img

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -1,1 +1,40 @@
 'use strict'
+
+let gElCanvas
+let gCtx
+
+function onInit() {
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+    addListeners()
+    renderMeme()
+}
+
+function renderMeme() {
+    const pos = getPenPos()
+    const meme = getMeme()
+    drawImg()
+    drawText('I sometimes eat Falafel', pos.x, pos.y)
+}
+
+function drawImg() {
+    const img = new Image()
+    img.src = getImg().url
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight)
+    }
+}
+
+function drawText(text, x, y) {
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = 'orange'
+
+    gCtx.fillStyle = 'lightsteelblue'
+
+    gCtx.font = '45px Arial'
+    gCtx.textAlign = 'center'
+    gCtx.textBaseline = 'middle'
+
+    gCtx.fillText(text, x, y)
+    gCtx.strokeText(text, x, y)
+}
