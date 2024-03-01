cf8390baa7a63ee91383f59a9ce4f30b9c174048
Yifaht Meir <yifahtmyers@gmail.com>
2024-03-01 17:07:34 +0200

    Display text on img on canvas

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -4,6 +4,7 @@ let gElCanvas
 let gCtx
 
 function onInit() {
+    renderGallery()
     gElCanvas = document.querySelector('canvas')
     gCtx = gElCanvas.getContext('2d')
     addListeners()
@@ -11,47 +12,29 @@ function onInit() {
 }
 
 function renderMeme() {
-    const InitX = gElCanvas.width / 2
-    const initY = 50;
     const meme = getMeme()
-    drawImg()
-    drawText(meme[0].lines[0].txt, InitX, initY)
+    drawImg(meme)
 }
 
-// function drawImg() {
-//     const img = new Image()
-//     img.src = getImg().url
-//     img.onload = () => {
-//         gCtx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight)
-//     }
-// }
-
-// function drawImg() {
-//     const elImg = document.querySelector('img')
-//     gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
-// }
-
-
-function drawImg() {
+function drawImg(meme) {
     const elImg = new Image()
-    elImg.src = getImg().url
-    elImg.onload = () =>
+    elImg.src = getImgs().find(img => img.id === meme.selectedImgId).url
+    elImg.onload = () => {
         gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+        drawText(meme.lines[0].txt, gElCanvas.width / 2, 50)
+    }
 }
 
 function drawText(text, x, y) {
-    gCtx.beginPath()
-    gCtx.lineWidth = 2
-    gCtx.strokeStyle = 'orange'
-    gCtx.fillStyle = 'lightsteelblue'
-    gCtx.font = '45px Arial'
+    gCtx.fillStyle = 'white'
+    gCtx.strokeStyle = 'black'
+    gCtx.font = '30px Arial'
     gCtx.textAlign = 'center'
-    gCtx.textBaseline = 'middle'
     gCtx.fillText(text, x, y)
     gCtx.strokeText(text, x, y)
 }
 
 function OnUpdateMeme(txt) {
-    const updatedMeme = updateMeme(txt)
+    updateMeme(txt)
     renderMeme()
 }
