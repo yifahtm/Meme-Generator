db1318090bad85a0ffbd0b9a6b51bd7259b1adf5
Yifaht Meir <yifahtmyers@gmail.com>
2024-03-03 08:20:17 +0200

    Add upload and share buttons

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -137,3 +137,74 @@ function onDownload(elLink) {
     elLink.href = content
 }
 
+
+function onSaveMemes() {
+    saveMemes()
+}
+
+function onLoadMemes() {
+    loadMemes()
+    //     gCtx.moveTo(gLine[0].x, gLine[0].y)
+    //     gCtx.beginPath()
+    //     gLine.forEach(pos => gCtx.lineTo(pos.x, pos.y))
+    //     gCtx.stroke()
+}
+
+function onUpload(ev) {
+    loadImageFromInput(ev, renderImg)
+}
+
+function loadImageFromInput(ev, onImageReady) {
+    const input = ev.target
+    const reader = new FileReader()
+
+    reader.onload = ev => {
+        const img = new Image()
+        img.src = ev.target.result
+        img.onload = () => onImageReady(img)
+    }
+    reader.readAsDataURL(input.files[0])
+}
+
+function renderImg(img) {
+    gElCanvas.height = (img.naturalHeight / img.naturalWidth) * gElCanvas.width
+    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+}
+
+function onUploadToFace() {
+    const imgDataUrl = gElCanvas.toDataURL('image/jpeg')
+
+    function onSuccess(uploadedImgUrl) {
+        const url = encodeURIComponent(uploadedImgUrl)
+        window.open(`https://www.facebook.com/sharer/sharer.php?u=${url}&t=${url}`)
+    }
+
+    doUploadImg(imgDataUrl, onSuccess)
+}
+
+function doUploadImg(imgDataUrl, onSuccess) {
+    const formData = new FormData()
+    formData.append('img', imgDataUrl)
+
+    const XHR = new XMLHttpRequest()
+    XHR.onreadystatechange = () => {
+        if (XHR.readyState !== XMLHttpRequest.DONE) return
+
+        if (XHR.status !== 200) return console.error('Error uploading image')
+        const { responseText: url } = XHR
+
+        onSuccess(url)
+    }
+
+    XHR.onerror = (req, ev) => {
+        console.error(
+            'Error connecting to server with request:',
+            req,
+            '\nGot response data:',
+            ev
+        )
+    }
+    XHR.open('POST', '//ca-upload.com/here/upload.php')
+    XHR.send(formData)
+}
+
